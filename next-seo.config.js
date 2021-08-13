97b5fb9a44377f50f902cf42bac6d56db2f58869
Sourabh Kumar <sourabhbgp1994@gmail.com>
2021-08-13 21:53:53 +0530

    initial setup done

---
+++ next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+  title: '',
+  description: '',
+  canonical: '',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: '',
+    site_name: '',
+    title: '',
+    description: '',
+  },
+}
