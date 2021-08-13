c1ce0413fb636c1835ddce4635174a040a497eda
Sourabh Kumar <sourabhbgp1994@gmail.com>
2021-08-13 23:36:27 +0530

    multi language config done

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'fr', 'hi'],
+    defaultLocale: 'en',
+    localeDetection: false, // Stop auto locales detection
+  },
+
+  trailingSlash: true,
 }
